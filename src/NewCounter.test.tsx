2c14acb43aa3f15cb7df7002397b39bc0c9ee116
Nicole-Rene Newcomb <n.newcomb@elsevier.com>
2024-07-10 09:51:45 -0700

    Created new files instead of deleting prior content in Counter.tsx and Counter.test.tsx files due to tutorial shifting to a Presentation Component approach that shifts responsibilities.

---
+++ src/NewCounter.test.tsx
@@ -0,0 +1,5 @@
+// NRN - this file created in place of deleting parts of the Counter.tsx file while
+// following the tutorial that wanted to shift to using a Presentation Component
+// approach that shifts certain responsibilities to the parent
+
+import React from "react";
