cab285c437be903d0e1a773882846c95710a3458
Nicole-Rene Newcomb <n.newcomb@elsevier.com>
2024-07-09 17:56:47 -0700

    Created Counter1 and Counter2 components to experiment with tests/lookups available within React Jest test framework. Upon further investigation, although ID is commonly used with CSS/HTML elements, React component testing prefers querying using elements "visible" to either human users or screen readers. These include screen.getByLabelText, .getByRole, .getByText. The screen.getByTestId can be used on the data-testid attribute.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import {Heading} from "./Heading";
+import Counter2 from "./Counter";
 
 export function label(name: string) : string {
     return `Hello ${name.toUpperCase()}`;
@@ -11,6 +12,7 @@ function App(): React.JSX.Element {
       <div>
           <Heading/>
           <h2>Hey There!</h2>
+          <Counter2 label={"Current"}/>
       </div>
   );
 }
