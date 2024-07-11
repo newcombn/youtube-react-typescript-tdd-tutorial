e5f582d0cb0d089618a800566128a7a2da96996e
Nicole-Rene Newcomb <n.newcomb@elsevier.com>
2024-07-11 15:50:22 -0700

    Created another version of counter with NewCounter that will be a "dumb" child component passed information from App parent following the Presentation Component approach. Also created the tests to ensure that NewCounter works properly with a click increment and shift click increment of 10. Used mock jest functions to check that functions were properly passed and called in NewCounter.

--- src/NewCounter.test.tsx
+++ src/NewCounter.test.tsx
@@ -3,3 +3,63 @@
 // approach that shifts certain responsibilities to the parent
 
 import React from "react";
+import { fireEvent, render, screen} from "@testing-library/react";
+import NewCounter from "./NewCounter";
+
+test("NewCounter render counter w/ default label", (): void => {
+    const handler: jest.Mock<any, any> = jest.fn();
+    render(<NewCounter count={0} onCounterIncrease={handler} />);
+    const label : HTMLElement = screen.getByTitle("Count");
+    expect(label).toBeInTheDocument();
+    const count : HTMLElement = screen.getByTitle("counter");
+    expect(count).toBeInTheDocument();
+});
+
+test("NewCounter render custom label/counter", (): void => {
+    const handler: jest.Mock<any, any> = jest.fn();
+    render(<NewCounter label={`Current`} count={0} onCounterIncrease={handler} />);
+    const label : HTMLElement = screen.getByTitle("Current");
+    expect(label).toBeInTheDocument();
+    const count : HTMLElement = screen.getByTitle("counter");
+    expect(count).toBeInTheDocument();
+});
+
+// NRN - can't query directly by ID. Must use data-testid in JSX/TSX and use either
+// screen.queryByTestId() or screen.getByTestId. The first requires typing
+// for possible null return value while the second doesn't. Overall, the recommendation
+// is to test by LabelText, Role, or other attributes that are "visible" to either
+// human viewers or screen readers. Testing by ID may mean you are missing important
+// attributes in the components.
+test("NewCounter render a label and counter", (): void => {
+    const handler: jest.Mock<any, any> = jest.fn();
+    render(<NewCounter count={0} onCounterIncrease={handler} />);
+    const label : HTMLElement = screen.getByTitle("Count");
+    expect(label).toBeInTheDocument();
+    const count: HTMLElement | null = screen.queryByTestId("counter");
+    expect(count).toBeInTheDocument();
+});
+
+test("NewCounter render custom label/counter 2", (): void => {
+    const handler: jest.Mock<any, any> = jest.fn();
+    render(<NewCounter label={`Current`} count={0} onCounterIncrease={handler} />);
+    const label : HTMLElement = screen.getByTitle("Current");
+    expect(label).toBeInTheDocument();
+    const count : HTMLElement = screen.getByTestId("counter");
+    expect(count).toBeInTheDocument();
+});
+
+// NRN - using ock jest test to check that function is passed/used
+test("should call the incrementer", (): void => {
+    const handler: jest.Mock<any, any> = jest.fn();
+    render(<NewCounter label={`Current`} count={0} onCounterIncrease={handler} />);
+    const label : HTMLElement = screen.getByTitle("Current");
+    expect(label).toBeInTheDocument();
+    const counter : HTMLElement = screen.getByTestId("counter");
+    expect(counter).toBeInTheDocument();
+    // NRN - this imitates the click event that will trigger mock function
+    fireEvent.click(counter);
+    // NRN - this checks that passed function has been called
+    expect(handler).toBeCalledTimes(1);
+    // NRN - here referencing shift true/false when function called
+    expect(handler).toBeCalledWith(false);
+});
--- src/NewCounter.tsx
+++ src/NewCounter.tsx
@@ -1,5 +1,33 @@
 // NRN - this file created in place of deleting parts of the Counter.tsx file while
 // following the tutorial that wanted to shift to using a Presentation Component
 // approach that shifts certain responsibilities to the parent
+// Counter1 and Counter2 are set up as smart components, whereas NewCounter
+// is set up as a dumb component that receives information from parent App
 
-import React, {Component} from 'react';
+import React from 'react';
+
+// NRN - create a type to indicate type of props passed to component
+export type NewCounterProps = {
+    label?: string,
+    count: number,
+    onCounterIncrease: (isShift: boolean) => void;
+};
+
+const NewCounter = ({label = "Count", count, onCounterIncrease}: NewCounterProps) => {
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        onCounterIncrease(event.shiftKey);
+    };
+
+    return (
+        <div>
+            <span title={label}>Counter</span>
+            <span data-testid="counter" title="counter" role="region"
+                  aria-live="polite" aria-label="Counter"
+                  onClick={handleClick} >
+					{count}
+				</span>
+        </div>
+    );
+}
+
+export default NewCounter;
